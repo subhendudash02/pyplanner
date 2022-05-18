f878fc66bc914422f4db7408c20f3b20d21adf94
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-05-19 00:40:45 +0530

    new operations in db and push notifs

--- db_ops.py
+++ db_ops.py
@@ -2,6 +2,7 @@ import subprocess as sp
 import sqlite3 as sql
 
 connect_db = sql.connect("util.db")
+cursor = connect_db.cursor()
 
 def remove_db(): # will be used to reset the application
     x = sp.run(["find", "util.db"], stdout=sp.DEVNULL, stderr=sp.DEVNULL)
@@ -11,10 +12,24 @@ def remove_db(): # will be used to reset the application
         sp.run(["rm", "util.db"])
 
 def create_table(name, attr, datatypes):
-    connect_db.execute("CREATE TABLE {}({} {}, {} {});"
-                    .format(name, attr[0], datatypes[0], attr[1], datatypes[1])
+    cursor.execute("CREATE TABLE {}({} {}, {} {}, {} {});"
+                    .format(name, attr[0], datatypes[0], attr[1], datatypes[1], attr[2], datatypes[2])
                     )
+    connect_db.commit()
 
 def insertInto_table(name, values):
-    connect_db.execute("INSERT INTO {} VALUES({}, '{}');".format(name, values[0], values[1]))
+    cursor.execute("INSERT INTO {} VALUES('{}', '{}', '{}');".format(name, values[0], values[1], values[2]))
+    connect_db.commit()
+
+def sort_table():
+    cursor.execute("SELECT unix from task ORDER BY unix ASC;")
+    connect_db.commit()
+
+def topmost_row():
+    time = cursor.execute("SELECT unix, name from task LIMIT 1;")
+    connect_db.commit()
+    return time.fetchall()
+
+def delete_row(unix):
+    cursor.execute("DELETE from task where unix = {};".format(unix))
     connect_db.commit()
--- terminal_connect.py
+++ terminal_connect.py
@@ -1,16 +1,37 @@
-import sqlite3 as sql
 import db_ops as ops
 import sys
+from datetime import datetime
+import time
+import subprocess
 
-connect_db = sql.connect("util.db") # will create and connect the database
 args = len(sys.argv)
 
+def push_notification(message):
+    subprocess.Popen(['notify-send', msg])
+    return
+
 try:
-    ops.create_table("task", ["sl_no", "name"], ["integer", "text"])
+    ops.create_table("task", ["name", "time", "unix"], ["text", "text", "bigint"])
 except:
     pass
 
 if args == 4:
-    ops.insertInto_table(sys.argv[1], [sys.argv[2], sys.argv[3]])
-else:
+    d = datetime.now()
+    new = datetime(d.year, d.month, d.day, int(sys.argv[3][0:2]), int(sys.argv[3][3:5]), 0)
+    unix = time.mktime(new.timetuple())
+    ops.insertInto_table(sys.argv[1], [sys.argv[2], sys.argv[3], unix])
+    ops.sort_table()
+elif args == 2 and sys.argv[1] == "reset":
+    ops.remove_db()
+elif args == 1:
     print("Set some tasks...")
+
+while True:
+    current_time = datetime.now()
+    sch_time = ops.topmost_row()
+    current_time = time.mktime(current_time.timetuple())
+    diff = int(sch_time[0][0]) - current_time
+    msg = str(sch_time[0][1])
+    if diff == 0:
+        push_notification(msg)
+        ops.delete_row(sch_time[0][0])
