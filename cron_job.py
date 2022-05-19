db35392a4ea2b77500289ee45ffb96e06022a5eb
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-05-19 22:36:04 +0530

    add cron job in Ubuntu

---
+++ cron_job.py
@@ -0,0 +1,11 @@
+from crontab import CronTab
+
+cron = CronTab(user="subhendu")
+
+def make_job(command, li):
+    job = cron.new(command)
+    job.minute.on(li[0])
+    job.hour.on(li[1])
+    job.day.on(li[2])
+    job.month.on(li[3])
+    cron.write()
