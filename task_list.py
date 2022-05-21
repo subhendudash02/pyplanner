7eab28f56b6b5b51babbc8cb0545cef04e1ecebb
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-05-21 15:12:55 +0530

    New Feature: List out task in tabular form

---
+++ task_list.py
@@ -0,0 +1,23 @@
+from rich.console import Console
+from rich.table import Table
+import db_ops as ops
+import os
+
+def print_tasks():
+    os.system("clear")
+    table = Table(title="\nYour Scheduled Tasks\n", expand=True, show_lines=True)
+
+    table.add_column("Sl.no", justify="center")
+    table.add_column("Task Name", justify="center")
+    table.add_column("Time", justify="center")
+
+    li = ops.show_tasks()
+    sl = 1
+
+    for i in li:
+        table.add_row(str(sl), i[0], i[1])
+        sl += 1
+
+    console = Console()
+    console.print(table)
+    print("\n")
--- terminal_connect.py
+++ terminal_connect.py
@@ -3,25 +3,32 @@ import sys
 from datetime import datetime
 import time
 import cron_job as cr
+import task_list as tl
 
 args = len(sys.argv)
 
 try:
-    ops.create_table("task", ["name", "time", "unix", "cron"], ["text", "text", "bigint", "text"])
+    ops.create_table("set_task", ["name", "time", "unix", "cron"], ["text", "text", "bigint", "text"])
 except:
     pass
 
 if args == 4:
-        d = datetime.now()
-        new = datetime(d.year, d.month, d.day, int(sys.argv[3][0:2]), int(sys.argv[3][3:5]), 0)
-        unix = time.mktime(new.timetuple())
-        cron = "{} {} {} {} *".format(sys.argv[3][3:5], sys.argv[3][0:2], d.day, d.month)
-        ops.insertInto_table(sys.argv[1], [sys.argv[2], sys.argv[3], unix, cron])
-elif args == 2 and sys.argv[1] == "reset":
-    ops.remove_db()
+    d = datetime.now()
+    new = datetime(d.year, d.month, d.day, int(sys.argv[3][0:2]), int(sys.argv[3][3:5]), 0)
+    unix = time.mktime(new.timetuple())
+    cron = "{} {} {} {} *".format(sys.argv[3][3:5], sys.argv[3][0:2], d.day, d.month)
+    ops.insertInto_table([sys.argv[2], sys.argv[3], unix, cron])
+    final_txt = "XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send Reminder '{}'".format(sys.argv[2])  
+    cr.make_job(final_txt, [sys.argv[3][3:5], sys.argv[3][0:2], d.day, d.month])
+
+elif args == 2:
+    if sys.argv[1] == "reset":
+        ops.remove_db()
+        cr.clear_jobs()
+        print("All the tasks are removed. Hence you will not receive notifications until you schedule a new task.")
+    elif sys.argv[1] == "show_tasks":
+        tl.print_tasks()
+    
 elif args == 1:
     print("Set some tasks...")
-
-final_txt = "XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send Reminder '{}'".format(sys.argv[2])
-
-cr.make_job(final_txt, [sys.argv[3][3:5], sys.argv[3][0:2], d.day, d.month])
+    print("Usage: python3 terminal_connect.py set_task <task_name> <time>")
