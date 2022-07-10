ceb580636234c2eb4e1097392f73e4b5d8ce5b3d
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-07-10 10:19:38 +0530

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
 from setuptools import setup
 
-setup(name="pyagenda")
+setup(
+    include_package_data=True,
+    name="pyagenda",
+    version="1.0.0",
+    description="pyagenda keeps track of all your activities and notifies you when they are due",
+    author="Subhendu Dash",
+    author_email="sdash29102@gmail.com",
+    license="MIT",
+    url="https://github.com/subhendudash02/pyagenda",
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+    keywords="to-do crontab sqlite3")
