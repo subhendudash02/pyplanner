6f8e2b1d92447d5fedbea5bcd54e86ff6cc86e28
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-10-02 14:55:17 +0530

    add contributing guidelines

--- appUtil/gui_app.py
+++ appUtil/gui_app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'app/gui.ui'
+# Form implementation generated from reading ui file 'gui.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.6
 #
